a4cd884c520add0fd4414df85f5fcb0b89618840
hslam <791874158@qq.com>
2020-07-02 11:26:11 +0800

    add net/rpc bench

---
+++ netrpc/server/server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"github.com/hslam/rpc-benchmark/service"
+	"log"
+	"net"
+	"net/rpc"
+)
+
+var network string
+var addr string
+
+func init() {
+	flag.StringVar(&network, "network", "tcp", "-network=tcp")
+	flag.StringVar(&addr, "addr", ":9999", "-addr=:9999")
+	flag.Parse()
+}
+func main() {
+	rpc.Register(new(service.Arith))
+	lis, err := net.Listen(network, addr)
+	if err != nil {
+		log.Fatalln("fatal error: ", err)
+	}
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(conn)
+	}
+}
