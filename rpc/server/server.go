0d81dfbf1d2a8c40a9914aa3bbed2495fe9dfb28
hslam <791874158@qq.com>
2020-09-28 12:07:13 +0800

    add poll

--- rpc/server/server.go
+++ rpc/server/server.go
@@ -9,15 +9,18 @@ import (
 var network string
 var addr string
 var codec string
+var poll bool
 
 func init() {
 	flag.StringVar(&network, "network", "tcp", "-network=tcp")
 	flag.StringVar(&addr, "addr", ":9999", "-addr=:9999")
 	flag.StringVar(&codec, "codec", "pb", "-codec=code")
+	flag.BoolVar(&poll, "poll", false, "-poll=false")
 	flag.Parse()
 }
 
 func main() {
 	rpc.Register(new(service.Arith))
+	rpc.SetPoll(poll)
 	rpc.Listen(network, addr, codec)
 }
